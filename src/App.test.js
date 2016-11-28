43fea04ca01dc9ab27d3d01a55821f42efbac119
evanfrawley <frawley@uw.edu>
2016-11-27 17:59:58 -0800

    Edited gitignore and added sections to App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,21 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+
+// Email Address tests
+
+
+// Name field test
+
+
+// Birthday field tests
+
+
+// The password and password confirmation fields
+
+
+// Form Reset Tests
+
+
+// Form Submit tests
